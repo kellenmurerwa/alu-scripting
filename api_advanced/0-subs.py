713e39199477be76fdbe7e0bbfd8385650453c31
Kells-design <kellenmurerwa724@gmail.com>
2024-07-05 10:27:45 -0700

    Task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""
-Returns the number of subscribers from a subreddit
-"""
+"""Return number of subscriber for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Set a custom header user-agent """
-    headers = {"User-Agent": "ALU-scripting API 0.1"}
-    url = "https://www.reddit.com/r/{}.json".format(subreddit)
 
-    try:
-        response = requests.get(url, headers=headers,
-                                timeout=30, allow_redirects=False)
-
-    except requests.exceptions.Timeout:
-        return "The request Timed out"
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        subscriber_number = (
-            json_data.get("data")
-            .get("children")[0]
-            .get("data")
-            .get("subreddit_subscribers")
-        )
+        subscriber_number = json_data.get('data').get(
+            'children')[0].get('data').get('subreddit_subscribers')
         return subscriber_number
-    elif response.status_code == 404:
-        return 0
     else:
         return 0
