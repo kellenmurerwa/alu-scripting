8158272ad1f7c4f644f86ab6c23470cbb23c74af
Kells-design <kellenmurerwa724@gmail.com>
2024-07-05 11:14:54 -0700

    Task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""Return number of subscriber for a given subreddit. """
-
+"""
+Returns the number of subscribers from a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Retrieves the number of subscribers for a given subreddit"""
-   headers = { "User-Agent": "ALU-scripting API 0.1"}
-    url = f"https://reddit.com/r/{subreddit}.json" 
+    """ Set a custom header user-agent """
+    headers = {"User-Agent": "ALU-scripting API 0.1"}
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
 
     try:
-        response = requests.get(url, headers=headers, timeout=30, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                timeout=30, allow_redirects=False)
 
     except requests.exceptions.Timeout:
-        return "The request timed out"
+        return "The request Timed out"
 
     if response.status_code == 200:
-        try:
-            json_data = response.json()
-            subscriber_number = json_data.get("data").get("children")[0].get("data").get("subreddit_subscribers")
-            return subscriber_number
-        except (AttributeError, KeyError):  
-            return 0
-
+        json_data = response.json()
+        subscriber_number = (
+            json_data.get("data")
+            .get("children")[0]
+            .get("data")
+            .get("subreddit_subscribers")
+        )
+        return subscriber_number
     elif response.status_code == 404:
         return 0
-
     else:
-        return 0  
+        return 0 
